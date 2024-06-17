0e7bdda9c4a1c18a0bd0b54c1a4bbd80267e34f1
Helena Rezende <99421906+GalaxyHG@users.noreply.github.com>
2024-06-17 16:21:04 +0000

    feito

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Sobre from './pages/Sobre'
 import Home from './pages/Home'
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} ></Route>
+                <Route path="/" element={ <Home /> }></Route>
+                <Route path="/sobre" element={ <Sobre /> }></Route>
+                {/* <Route path="/projetos" element={ <Projetos /> }></Route>
+                <Route path="/contatos" element={ <Contatos /> }></Route>
+                <Route path="*" element={ <Page404 /> }></Route> */}
             </Routes>
     </BrowserRouter>
     )
