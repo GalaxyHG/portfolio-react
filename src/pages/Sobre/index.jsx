e262fb30ce25f636621fbdf03d4183622841ec88
Helena Rezende <99421906+GalaxyHG@users.noreply.github.com>
2024-06-17 23:36:13 +0000

    icons change

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,13 +1,15 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import Container from '../../components/Container'
+import { IoLogoHtml5 } from "react-icons/io5";
 
 function Sobre() {
     return (
         <>
             <Header />
             <Container>
-                <h2>Sobre</h2>
+                <h2>Sobre <IoLogoHtml5 /></h2>
+                
             </Container>
             <Footer />
         </>
