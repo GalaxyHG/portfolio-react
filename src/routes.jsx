8ad231ebe959e890744311aa2979091c5b1fc3b2
Helena Rezende <99421906+GalaxyHG@users.noreply.github.com>
2024-06-18 12:00:59 +0000

    Projetos page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,30 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
-// import Projetos from './pages/Projetos'
+import Projetos from './pages/Projetos'
 import Contatos from './pages/Contatos'
 import Page404 from './pages/Page404'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Container from './components/Container'
+import PageBase from './pages/PageBase'
 
 function AppRoutes() {
     return (
-        <>
         <BrowserRouter>
-            <Header />
-            <Container>
-                <Routes>
-                    <Route path="/" element={ <Home /> }></Route>
+            <Routes>
+                <Route path='/' element={ <PageBase /> }>
+                    <Route index element={ <Home /> }></Route>
                     <Route path="/sobre" element={ <Sobre /> }></Route>
-                    {/* <Route path="/projetos" element={ <Projetos /> }></Route> */}
+                    <Route path="/projetos" element={ <Projetos /> }></Route>
                     <Route path="/contatos" element={ <Contatos /> }></Route>
                     <Route path="*" element={ <Page404 /> }></Route>
-                </Routes>
-            </Container>
-            <Footer />
+                </Route>
+            </Routes>
         </BrowserRouter>
-        </>
     )
 }
 
